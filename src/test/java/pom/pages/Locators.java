5e7c3de974b790726a5bd18270fadd16bd0e9942
ZanetaB <zzanule@gmail.com>
2023-03-17 18:12:23 +0200

    Added Locators class

---
+++ src/test/java/pom/pages/Locators.java
@@ -0,0 +1,14 @@
+package pom.pages;
+
+import org.openqa.selenium.By;
+
+public class Locators {
+
+    public static class Delona {
+
+        public static class HomePage {
+
+            public static By buttonManoPaskyra = By.xpath("//a[@title='Mano paskyra']");
+        }
+    }
+}
