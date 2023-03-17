3da294a833caafbccee2719972d99597bccc9ba0
ZanetaB <zzanule@gmail.com>
2023-03-17 18:02:12 +0200

    Added Driver class

---
+++ src/test/java/pom/utilities/Driver.java
@@ -0,0 +1,39 @@
+package pom.utilities;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
+public class Driver {
+
+    private static WebDriver driver;
+
+    public static void setDriver() {
+
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("start-maximized");
+        options.addArguments("--force-device-scale-factor=0.65");
+
+        driver = new ChromeDriver(options);
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+    }
+
+    public static WebDriver getDriver() {
+        return driver;
+    }
+
+    public static void closeDriver() {
+        driver.quit();
+    }
+
+}
+
