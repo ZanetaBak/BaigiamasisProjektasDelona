2f221e330d531f28b4f9fcae4f6a7b09f9808656
ZanetaB <zzanule@gmail.com>
2023-03-20 13:36:34 +0200

    Annotations updated in BaseTest class

--- src/test/java/pom/tests/BaseTest.java
+++ src/test/java/pom/tests/BaseTest.java
@@ -1,9 +1,7 @@
 package pom.tests;
 
 
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Listeners;
+import org.testng.annotations.*;
 import pom.pages.Common;
 import pom.utilities.TestListener;
 
