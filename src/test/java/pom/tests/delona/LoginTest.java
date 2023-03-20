bc3784da791d016ff81e071e17281e57d1083856
ZanetaB <zzanule@gmail.com>
2023-03-20 13:31:06 +0200

    Two Login tests added to LoginTest class

--- src/test/java/pom/tests/delona/LoginTest.java
+++ src/test/java/pom/tests/delona/LoginTest.java
@@ -1,6 +1,7 @@
 package pom.tests.delona;
 
 
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pom.pages.delona.LoginPage;
@@ -17,8 +18,42 @@ public class LoginTest extends BaseTest {
     @Test
     public void testLoginWithExistingUserData() {
 
-        LoginPage.openFormManoPaskyra();
+        String emailAddress = "zaneta.bakanauskiene@gmail.com";
+        String password = "vcsbaigiamasis";
+        String expectedSurname = "Bakanauskienė";
+        String actualSurname;
 
+        LoginPage.openFormMyAccount();
+
+        LoginPage.enterEmailAddress(emailAddress);
+        LoginPage.enterPassword(password);
+        LoginPage.clickButtonLogIn();
+
+        LoginPage.clickButtonChangeAccountInfo();
+        actualSurname = LoginPage.readDisplayText();
+
+        Assert.assertEquals(actualSurname, expectedSurname);
     }
 
+    @Test
+    public void testLogInAttemptWithNonExistingAccountInfo() {
+
+        String emailAddress = "kazkas@email.com";
+        String password = "password";
+        String expectedMessage = "Įspėjimas";
+        String actualMessage;
+
+        LoginPage.openFormMyAccount();
+
+        LoginPage.enterEmailAddress(emailAddress);
+        LoginPage.enterPassword(password);
+        LoginPage.clickButtonLogIn();
+
+        actualMessage = LoginPage.readDisplayAlert();
+
+        Assert.assertTrue(
+                actualMessage.contains(expectedMessage),
+                String.format("Actual: %s, Expected: %s", actualMessage, expectedMessage)
+        );
+    }
 }
