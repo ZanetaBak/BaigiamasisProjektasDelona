0e987ef7b6c82268e935673a1ede4c71948d2c3a
ZanetaB <zzanule@gmail.com>
2023-03-20 13:40:43 +0200

    Updated xpaths

--- src/test/java/pom/pages/Locators.java
+++ src/test/java/pom/pages/Locators.java
@@ -6,9 +6,36 @@ public class Locators {
 
     public static class Delona {
 
+
         public static class HomePage {
 
-            public static By buttonManoPaskyra = By.xpath("//a[@title='Mano paskyra']");
+            public static By buttonMyAccount = By.xpath("//a[@title='Mano paskyra']");
+        }
+
+
+        public static class LoginInfo {
+
+            public static By inputEmailAddress = By.xpath("//input[@name='email']");
+
+            public static By inputPassword = By.xpath("//input[@name='password']");
+
+            public static By buttonLogIn = By.xpath("//input[@value='Prisijungti']");
+
+            public static By displayAlertMessage = By.xpath(
+                    "//div[@class='alert alert-danger alert-dismissible']"
+            );
+        }
+
+
+        public static class AccountInformation {
+
+            public static By buttonChangeAccountInfo = By.xpath(
+                    "//*[@id='content']/ul[1]/li[1]/a"
+            );
+
+            public static By displaySurname = By.xpath(
+                    "//input[@value='Bakanauskienė' and @id='input-lastname']"
+            );
         }
     }
 }
