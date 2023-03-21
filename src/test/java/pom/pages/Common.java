2de34f24729df03bdc810775ff27d8269ae65d16
ZanetaB <zzanule@gmail.com>
2023-03-21 15:00:25 +0200

    Added new methods

--- src/test/java/pom/pages/Common.java
+++ src/test/java/pom/pages/Common.java
@@ -1,10 +1,10 @@
 package pom.pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import pom.utilities.Driver;
-
-import java.util.List;
 
 public class Common {
 
@@ -30,7 +30,7 @@ public class Common {
         element.sendKeys(message);
     }
 
-    private static WebElement getElement(By locator) {
+    public static WebElement getElement(By locator) {
         return Driver.getDriver().findElement(locator);
     }
 
@@ -41,4 +41,19 @@ public class Common {
     public static String getElementAttributeParameter(String attributeName, By locator) {
         return getElement(locator).getAttribute(attributeName);
     }
-}
+    public static void selectOptionByValue(String attributeValue, By locator) {
+        WebElement element = getElement(locator);
+        Select select = new Select(element);
+        select.selectByValue(attributeValue);
+    }
+
+    public static void setAttributeValue(By locator, String value){
+        JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
+        js.executeScript(
+                "arguments[0].setAttribute(arguments[1],arguments[2])",
+                getElement(locator),
+                "value",
+                value
+        );
+    }
+    }
